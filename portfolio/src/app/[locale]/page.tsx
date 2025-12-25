218db657c76fd36094603ad83b916c12613126ab
Sabeeh <sabeehhassan29@gmail.com>
2025-12-25 20:26:26 +0500

    add hearder

--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -5,12 +5,16 @@ import About from '@/components/sections/About';
 import  Experience  from '@/components/sections/Experience';
 import  Education  from '@/components/sections/Educations';
 import Contact from '@/components/sections/Contact';
+import {Header} from '@/components/layout//Header';
+
+
 export default async function HomePage() {
   // const t = await getTranslations('hero');
 
   return (
 
     <>
+      <Header />
       <Hero />
       <About />
       <Experience />
